3c92310e06108948d8871d8117d221da2bef3626
urriellu <urriellu@5f77b83f-c0d1-4471-be93-57dfde23853e>
2009-12-30 18:43:32 +0000

    New defines: AllDigitalInputs, AllDigitalOutputs, DigitalBits()

--- trunk/lib/MBP.h
+++ trunk/lib/MBP.h
@@ -8,5 +8,8 @@
 
 #define DigitalInput 1
 #define DigitalOutput 0
+#define AllDigitalInputs 0xFF
+#define AllDigitalOutputs 0x00
 
+#define ConfigBits(bits) typedef unsigned int config; config at 0x2007 __CONFIG = bits
 #define ConfigBits1(bits) typedef unsigned int config; config at _CONFIG1 __CONFIG = bits
