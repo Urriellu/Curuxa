e30da2749ef32c8bfc44a030eba929a6fafa7c9c
urriellu <urriellu@5f77b83f-c0d1-4471-be93-57dfde23853e>
2010-01-01 17:47:08 +0000

    New set of extensions for Project[]

---
+++ trunk/CuruxaIDE/ProjectArrayExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CuruxaIDE {
+	public static class ProjectArrayExtensions {
+		/// <summary>
+		/// Return a project from an array by its given name
+		/// </summary>
+		public static Project GetByName(this Project[] prjs, string Name) {
+			foreach(Project prj in prjs) {
+				if(prj.Name == Name) return prj;
+			}
+			throw new ArgumentException("Unknown project");
+		}
+	}
+}
