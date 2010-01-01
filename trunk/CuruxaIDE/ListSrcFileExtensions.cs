3e54d67278d61c964938fc6d407c3b75dd3b9430
urriellu <urriellu@5f77b83f-c0d1-4471-be93-57dfde23853e>
2010-01-01 17:46:34 +0000

    New set of extensions for List<SrcFile>

---
+++ trunk/CuruxaIDE/ListSrcFileExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CuruxaIDE {
+	public static class ListSrcFileExtensions {
+		/// <summary>
+		/// Checks whether a file name exists in the collection or not
+		/// </summary>
+		public static bool ContainsFileName(this List<SrcFile> sources, string FileName) {
+			foreach(SrcFile src in sources) {
+				if(src.FileName == FileName) return true;
+			}
+			return false;
+		}
+
+		/// <summary>
+		/// Retrieves a SrcFile from the collection, given its FileName
+		/// </summary>
+		public static SrcFile GetByFileName(this List<SrcFile> sources, string FileName) {
+			foreach(SrcFile src in sources) {
+				if(src.FileName == FileName) return src;
+			}
+			throw new ArgumentException("File name \"" + FileName + "\" not found in List<SrcFile>");
+		}
+	}
+}
