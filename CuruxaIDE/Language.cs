3de7e29e87663dab7fe1274c4e3ca2bf301cf87e
urriellu <urriellu@curuxa.org>
2010-01-02 19:56:57 +0000

    "Text" is now a supported language. New method: GetFromExtension()

--- CuruxaIDE/Language.cs
+++ CuruxaIDE/Language.cs
@@ -9,6 +9,11 @@ namespace CuruxaIDE {
 	/// </summary>
 	public enum Language {
 		/// <summary>
+		/// Plain text
+		/// </summary>
+		Text,
+
+		/// <summary>
 		/// Microchip PIC Assembly Language
 		/// </summary>
 		PicAsm,
@@ -20,18 +25,32 @@ namespace CuruxaIDE {
 	}
 
 	public static class LanguageExtensions {
+		static Dictionary<Language, string> LangExtensions = new Dictionary<Language, string>();
+
+		static LanguageExtensions() {
+			LangExtensions.Add(Language.Text, "txt");
+			LangExtensions.Add(Language.C, "c");
+			LangExtensions.Add(Language.PicAsm, "asm");
+		}
+
+		/// <summary>
+		/// Get the language, given a extension
+		/// </summary>
+		/// <param name="extension"></param>
+		/// <returns></returns>
+		public static Language GetFromExtension(string extension) {
+			extension = extension.ToLowerInvariant();
+			foreach(KeyValuePair<Language, string> pair in LangExtensions) {
+				if(pair.Value == extension) return pair.Key;
+			}
+			return Language.Text;
+		}
+
 		/// <summary>
 		/// Gets the default extension for the given programming language (just the extension, without point)
 		/// </summary>
 		public static string GetExtension(this Language lang) {
-			switch(lang) {
-				case Language.C:
-					return "c";
-				case Language.PicAsm:
-					return "asm";
-				default:
-					throw new NotImplementedException();
-			}
+			return LangExtensions[lang];
 		}
 
 		/// <summary>
