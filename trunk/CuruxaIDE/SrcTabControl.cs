eed7916fdd34f4d7dd39d9184e75e0408a23d467
urriellu <urriellu@5f77b83f-c0d1-4471-be93-57dfde23853e>
2010-01-02 19:58:54 +0000

    New WinForms control: SrcTabControl

---
+++ trunk/CuruxaIDE/SrcTabControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CuruxaIDE {
+	/// <summary>
+	/// Managed a set of tabs containing source code editors
+	/// </summary>
+	public partial class SrcTabControl:TabControl {
+		Dictionary<SrcFile, SrcTabPage> Tabs = new Dictionary<SrcFile, SrcTabPage>();
+
+		public SrcTabControl():base() {
+		}
+
+		/// <summary>
+		/// Opens a source file. It doesn't matter if the source file is already open
+		/// </summary>
+		public void OpenSrc(SrcFile src) {
+			if(!Tabs.ContainsKey(src)) {
+				SrcTabPage NewTab = new SrcTabPage(src);
+				Tabs.Add(src, NewTab);
+				TabPages.Add(NewTab);
+			}
+			SelectedTab = Tabs[src];
+		}
+	}
+}
