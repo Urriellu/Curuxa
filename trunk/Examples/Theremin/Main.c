90b4a11ddb633da903fd044123e1d8e3b4d316fe
urriellu <urriellu@5f77b83f-c0d1-4471-be93-57dfde23853e>
2010-02-06 20:13:58 +0000

    Minor updates

--- trunk/Examples/Theremin/Main.c
+++ trunk/Examples/Theremin/Main.c
@@ -1,11 +1,35 @@
-﻿//Default template
+﻿/*==================================================================
+* Simple digital Theremin using MBP8, AO-SPK and SIDST-GP2
+*
+* Plug AO-SPK to GP5 (pin 2).
+* Plug SIDST-GP2 to AN3 (pin 3).
+*
+* http://curuxa.org
+*=================================================================*/
 
-#include <MBPxx.h>
+#include <MBP8.h>
 
-ConfigBits1(_CP_OFF & _LVP_OFF & _WDT_OFF & _INTRC_IO);
+ConfigBits(_CPD_OFF & _CP_OFF & _BOD_OFF  & _MCLRE_OFF & _PWRTE_ON & _WDT_OFF & _INTOSCIO);
+
+#define Spk GP51
+
+static void isr(void) interrupt 0 (){
+	Spk=!Spk;
+	TMR0IF=0;
+}
 
 void main() {
-	//configuration here...
+	//setup ADC
+	AdcDisable();
+	EnableAN3();
+	TRISIO5=DigitalOutput;
+
+	//setup timer
+	PSA=0;
+	T0CS=0;
+	TMR0IF=0;
+	GIE=1;
+	TMR0IE=1;
 
 	while(1){
 		//do something here...
