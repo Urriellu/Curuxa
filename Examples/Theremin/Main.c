5ff727a426d8c34bcb32eddb5a33e55cb392cc1c
urriellu <urriellu@curuxa.org>
2010-02-06 19:11:52 +0000

    New example: Theremin

---
+++ Examples/Theremin/Main.c
@@ -0,0 +1,13 @@
+﻿//Default template
+
+#include <MBPxx.h>
+
+ConfigBits1(_CP_OFF & _LVP_OFF & _WDT_OFF & _INTRC_IO);
+
+void main() {
+	//configuration here...
+
+	while(1){
+		//do something here...
+	}
+}
