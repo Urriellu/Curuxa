91250602df47eb2843338003f3bcb302f8a714ef
urriellu <urriellu@curuxa.org>
2010-02-07 21:03:50 +0000

    Updating macros

--- lib/MBP8.h
+++ lib/MBP8.h
@@ -8,6 +8,10 @@
 
 #define ADC_MAX_VALUE 1024
 
+#define Interrupt void Intr(void) __interrupt 0
+#define interrupt Interrupt
+#define ISR Interrupt
+
 //Disable Analog-to-Digital converter
 void AdcDisable(){
 	ADON=0;
@@ -16,8 +20,8 @@ void AdcDisable(){
 
 //Set AN0 as analog input, and use it as the source for the ADC conversion
 void EnableAN3(){
-	TRISIO4=DigitalInput;
-	ANSEL&=0b00001000;
+	SetBit(TRISIO, 4);
+	SetBit(ANSEL, 3);
 	CHS2=0;
 	CHS1=0;
 	CHS0=0;
