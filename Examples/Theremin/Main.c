522c9df2d8bd623a5c31f0dddcb8cbd3f9db864d
urriellu <urriellu@curuxa.org>
2010-02-08 22:07:01 +0000

    Lots of updates. It works (more os less) now.

--- Examples/Theremin/Main.c
+++ Examples/Theremin/Main.c
@@ -8,43 +8,59 @@
 *=================================================================*/
 
 #include <MBP8.h>
+#include <GP2.h>
 
 ConfigBits(_CPD_OFF & _CP_OFF & _BOD_OFF  & _MCLRE_OFF & _PWRTE_ON & _WDT_OFF & _INTOSCIO);
 
 #define Spk GP5
 
+unsigned int8 preload;
+
 Interrupt {
 	Spk=!Spk;
-	PR2=0;
-	TMR2IF=0;
+	TMR0=preload;
+	T0IF=0;
 }
 
-void main() {
+void Setup() {
 	//setup ADC
-	//AdcDisable();
-	//EnableAN3();
+	AdcDisable();
+	EnableAN3();
 
-	//setup speaker
-	ClearBit(TRISIO, 5);
-	Spk=1;
-
+	SetDigitalOutput(5);
+        SetIntosc8MHz();
+	
 	//setup timer
-	//PIC12F683 runs at 4MHz by default
+	//PIC12F683 runs by default at 4MHz
 	//Fosc=4MHz, Tosc=250ns, Tcy=1us
 	//for a 440Hz tone whe need a 880Hz timer frequency. That's 1136 instructions per cycle
-	//...1:1 postscaler (TOUTPS=0b0000).  1:16 prescaler (T2CKPS=0b10). PR2=71
-	TOUTPS3=0;
-	TOUTPS2=0;
-	TOUTPS1=0;
-	TOUTPS0=0;
-	T2CKPS1=1;
-	T2CKPS0=0;
-	PR2=71;
-	TMR2IF=0;
-	GIE=1;
-	TMR2IE=1;
-	TMR2ON=1;
+	//...1:8 prescaler (PS=0b010). 142 TMR0 counts  per cycle. preload=114	
+	PSA=0; //assign prescaler to Timer0
+	T0CS=0; //clock source=internal oscillator
+	PS2=0; //prescaler... 
+	PS1=1;
+	PS0=0;
+	T0IF=0;
+	GIE=1; //enable global interrupts
+	T0IE=1; //enable TMR0 interrupt
+}
+
+
+
+void main() {
+	unsigned int16 ADC=0;
+	Setup();
 
 	while(1) {
+		ADC=AdcMeasure();
+		if(ADC<100) {
+			//player's hand too far away, do nothing
+			TMR0=preload;
+		} else {
+			ADC-=154;
+			ADC>>=1;
+			ADC+=114;
+			preload=(unsigned int8)ADC;
+		}
 	}
 }
