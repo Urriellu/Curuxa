3ffacce43d41306ae41c628a53119932bf757eca
urriellu <urriellu@5f77b83f-c0d1-4471-be93-57dfde23853e>
2010-02-08 22:05:08 +0000

    New macro: SetDigitalInput(). New functions: EnableAN2(), AdcMeasure(), SetIntosc8MHz(). Fixed: EnableAN3().

--- trunk/lib/MBP8.h
+++ trunk/lib/MBP8.h
@@ -12,17 +12,42 @@
 #define interrupt Interrupt
 #define ISR Interrupt
 
+#define SetDigitalOutput(pin) ClearBit(TRISIO,pin)
+
 //Disable Analog-to-Digital converter
 void AdcDisable(){
 	ADON=0;
 	ANSEL=0;
 }
 
-//Set AN0 as analog input, and use it as the source for the ADC conversion
+//Set AN2 as analog input, and use it as the source for the ADC conversion
+void EnableAN2(){
+	SetBit(TRISIO, 2);
+	ANS2=1;
+	CHS1=1;
+	CHS0=0;
+}
+
+//Set AN3 as analog input, and use it as the source for the ADC conversion
 void EnableAN3(){
 	SetBit(TRISIO, 4);
-	SetBit(ANSEL, 3);
-	CHS2=0;
-	CHS1=0;
-	CHS0=0;
+	ANS3=1;
+	CHS1=1;
+	CHS0=1;
+}
+
+//Synchcronously get the analog value on the active analog input. It waits doing nothing until the measurement is done
+unsigned int16 AdcMeasure(){
+	ADFM=1;
+	ADON=1;
+	GO=1;
+	while(NOT_DONE) /*do nothing*/;
+	return ADRESH << 8 | ADRESL;
+}
+
+//Set the internal oscillator to 8MHz
+void SetIntosc8MHz(){
+	IRCF2=1;
+	IRCF1=1;
+	IRCF0=1;
 }
