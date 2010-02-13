b1d19e3e262458f7e60c3baed269241df85fd452
urriellu <urriellu@curuxa.org>
2010-02-13 22:13:37 +0000

    Supporting new microcontroller: PIC16F887. New Extension Method: GetMainBoard().

--- CuruxaIDE/Microcontroller.cs
+++ CuruxaIDE/Microcontroller.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Reflection;
 
 namespace CuruxaIDE {
 	/// <summary>
@@ -10,6 +8,19 @@ namespace CuruxaIDE {
 	public enum Microcontroller {
 		PIC12F683,
 		PIC16F688,
-		PIC16F88
+		PIC16F88,
+		PIC16F887
+	}
+
+	public static class MicrocontrollerExtensions {
+		/// <summary>
+		/// Gets the Main Board which uses the given Microcontroller. Returns null if the microcontroller is not used in Curuxa
+		/// </summary>
+		public static MainBoard? GetMainBoard(this Microcontroller MCU) {
+			foreach(MainBoard MB in Enum.GetValues(typeof(MainBoard))) {
+				if(MB.GetMCU() == MCU) return MB;
+			}
+			return null;
+		}
 	}
 }
