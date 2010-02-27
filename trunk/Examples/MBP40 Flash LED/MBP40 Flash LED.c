2bd49ee0d6bb3649539e7ce414ba49cc6dcc4fcc
urriellu <urriellu@5f77b83f-c0d1-4471-be93-57dfde23853e>
2010-02-27 21:01:31 +0000

    Small updates

--- trunk/Examples/MBP40 Flash LED/MBP40 Flash LED.c
+++ trunk/Examples/MBP40 Flash LED/MBP40 Flash LED.c
@@ -12,13 +12,13 @@
 
 #include <MBP40.h>
 
-ConfigBits1(_CP_OFF & _DEBUG_OFF & _WRT_PROTECT_OFF & _CPD_OFF & _LVP_OFF & _BODEN_OFF & _MCLR_OFF & _PWRTE_ON & _WDT_OFF & _INTRC_IO);
+ConfigBits1(_CP_OFF & _DEBUG_OFF & _FCMEN_OFF  & _IESO_OFF & _CPD_OFF & _LVP_OFF & _BOR_OFF & _MCLRE_OFF & _PWRTE_ON & _WDT_OFF & _INTOSCIO);
 
 #define OSC_8MHz
 #include <Delays.h>
 
 void main() {
-
+	SetIntosc8MHz();
 	AdcDisable();
 
 	TRISA=AllDigitalOutputs;
