87bcdb64946e2e294cfde86898e653b5e3a37efd
urriellu <urriellu@5f77b83f-c0d1-4471-be93-57dfde23853e>
2010-04-10 23:59:17 +0000

    New class: StringExtensions

---
+++ trunk/CuruxaIDE/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CuruxaIDE {
+	public static class StringExtensions {
+		public static bool IsNumber(this string str) {
+			foreach(char c in str) {
+				if(!char.IsNumber(c)) return false;
+			}
+			return true;
+		}
+
+		/// <summary>
+		/// Remove all text from the given value till the end
+		/// </summary>
+		public static string RemoveFrom(this string str, string value) {
+			if(str.Contains(value)) return str.Remove(str.IndexOf(value));
+			else return str;
+		}
+		
+		/// <summary>
+		/// Remove all text from the given value till the end
+		/// </summary>
+		public static string RemoveFrom(this string str, char value) {
+			return RemoveFrom(str, value.ToString());
+		}
+	}
+}
