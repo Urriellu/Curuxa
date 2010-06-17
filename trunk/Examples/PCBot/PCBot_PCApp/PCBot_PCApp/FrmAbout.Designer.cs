c56aa53a972e4f62fcd381b42bb6bcdf0d37ab61
urriellu <urriellu@5f77b83f-c0d1-4471-be93-57dfde23853e>
2010-06-17 19:59:07 +0000

    New "About" window

---
+++ trunk/Examples/PCBot/PCBot_PCApp/PCBot_PCApp/FrmAbout.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace PCBot_PCApp {
+	partial class FrmAbout {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(268, 99);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = "Graphical interface for controlling a two-wheeled robot from a computer.\r\n\r\nAutho" +
+				"r: Adrián Bulnes, aka Urriellu\r\n\r\nhttp://community.curuxa.org\r\nhttp://urriellu.n" +
+				"et";
+			this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+			// 
+			// FrmAbout
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(292, 123);
+			this.Controls.Add(this.textBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "FrmAbout";
+			this.Text = "About...";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
