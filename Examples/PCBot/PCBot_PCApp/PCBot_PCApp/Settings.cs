e30aaaa20c4796ed334b23f9c8878344d55ac17f
urriellu <urriellu@curuxa.org>
2010-06-17 20:00:46 +0000

    New variable: RobotID

--- Examples/PCBot/PCBot_PCApp/PCBot_PCApp/Settings.cs
+++ Examples/PCBot/PCBot_PCApp/PCBot_PCApp/Settings.cs
@@ -7,5 +7,6 @@ namespace PCBot_PCApp {
 	public static class Settings {
 		public static string SpName = "/dev/ttyUSB0";
 		public static int BaudRate = 9600;
+		public static byte RobotID = 13;
 	}
 }
