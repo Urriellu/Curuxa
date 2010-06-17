f164d76fc9cab6711071983a7df60232efdf5886
urriellu <urriellu@curuxa.org>
2010-06-17 19:59:07 +0000

    New "About" window

---
+++ Examples/PCBot/PCBot_PCApp/PCBot_PCApp/FrmAbout.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PCBot_PCApp {
+	public partial class FrmAbout:Form {
+		public FrmAbout() {
+			InitializeComponent();
+		}
+
+		private void textBox1_Enter(object sender, EventArgs e) {
+			textBox1.Select(0, 0);
+			this.Focus();
+		}
+	}
+}
