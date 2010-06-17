f54827919cccb9ecfb2e0581ed7ceafebd71c00c
urriellu <urriellu@curuxa.org>
2010-06-17 10:45:41 +0000

    Supporting interrupts. ANSELH also exists.

--- lib/MBP40.h
+++ lib/MBP40.h
@@ -6,12 +6,16 @@
 #include <pic16f887.h>
 #include <MBP.h>
 
+#define Interrupt static void isr(void) interrupt 0
+#define ISR Interrupt
+
 #define ADC_MAX_VALUE 1024
 
 //Disable Analog-to-Digital converter
 void AdcDisable(){
 	ADON=0;
 	ANSEL=0;
+	ANSELH=0;
 }
 
 //Set the internal oscillator to 8MHz
