08d805321903167586e34e6efc3752ce9e800ca5
urriellu <urriellu@curuxa.org>
2010-06-19 10:25:35 +0000

    Moving defines to main file. Receiving base movement information from PC.

--- Examples/PCBot/MCU/Communication.h
+++ Examples/PCBot/MCU/Communication.h
@@ -1,44 +1,4 @@
-﻿// list of control codes. This is the first byte transmitted through the serial port and indicates the operation being done
-
-//#define CcRESERVED 0
-#define CcAuthID 1
-#define CcSetLight 10
-#define CcSetBaseMovement 11
-#define CcStatusLight 100
-#define CcStatusBaseMovement 101
-#define CcStatusBumper 102
-//#define CcRESERVED 255
-
-
-
-//==========================================================================
-// Codes used by individual operations
-
-// CcAuthID - Identification
-#define CccAuthID 13
-
-// CcSetLight, CcStatusLight - Receive/send status of a light
-#define CccLightFrontLeftOn 50
-#define CccLightFrontLeftOff 51
-#define CccLightFrontRightOn 52
-#define CccLightFrontRightOff 53
-
-// CcSetBaseMovement, CcStatusBaseMovement - Receive/send status of base movement
-#define CccBaseMvFwd 20
-#define CccBaseMvStop 21
-#define CccBaseMvBck 22
-#define CccBaseTurnL 23
-#define CccBaseTurnR 24
-#define CccBaseRotateL 25
-#define CccBaseRotateR 26
-
-// CcStatusBumper - Send status of a bumper
-#define CccBumperFrontLeftPressed 70
-#define CccBumperFrontLeftReleased 71
-#define CccBumperFrontRightPressed 72
-#define CccBumperFrontRightReleased 73
-
-// temp variable to store received data
+﻿// temp variable to store received data
 unsigned int8 Rcv;
 
 void Authenticate(){
@@ -65,6 +25,34 @@ void SetLight(){
 	}
 }
 
+//Received an instruction to set the movement of the base
+void SetBaseMv(){
+	Rcv=ReadSPWait();
+	switch(Rcv) {
+		case CccBaseMvFwd:
+			BaseMvFwd();
+			break;
+		case CccBaseMvStop:
+			BaseStop();
+			break;
+		case CccBaseMvBck:
+			BaseMvBck();
+			break;
+		case CccBaseTurnL:
+			BaseTurnL();
+			break;
+		case CccBaseTurnR:
+			BaseTurnR();
+			break;
+		case CccBaseRotateL:
+			BaseRotateL();
+			break;
+		case CccBaseRotateR:
+			BaseRotateR();
+			break;
+	}
+}
+
 // Parse any kind of data received
 void ReceiveData() {
 	Rcv=ReadSP();
@@ -75,6 +63,9 @@ void ReceiveData() {
 				break;
 			case CcSetLight:
 				SetLight();
+				break;
+			case CcSetBaseMovement:
+				SetBaseMv();
 				break;
 		}
 	}
