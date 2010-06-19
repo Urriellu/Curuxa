40e319ef8e9d578bb073af114c661cf7241f02b2
urriellu <urriellu@curuxa.org>
2010-06-19 10:29:40 +0000

    Sending and receiving base movement information. New interactive mode (mouse and keyboard control).

--- Examples/PCBot/PCBot_PCApp/PCBot_PCApp/FrmMainWindow.Designer.cs
+++ Examples/PCBot/PCBot_PCApp/PCBot_PCApp/FrmMainWindow.Designer.cs
@@ -39,6 +39,21 @@
 			this.BtnAbout = new System.Windows.Forms.Button();
 			this.BtnLightFrontOn = new System.Windows.Forms.Button();
 			this.BtnLightFrontOff = new System.Windows.Forms.Button();
+			this.BtnBaseMvFwd = new System.Windows.Forms.Button();
+			this.ImgBaseMvFwd = new System.Windows.Forms.PictureBox();
+			this.BtnBaseMvTurnL = new System.Windows.Forms.Button();
+			this.BtnBaseMvTurnR = new System.Windows.Forms.Button();
+			this.BtnBaseMvRotateR = new System.Windows.Forms.Button();
+			this.BtnBaseMvRotateL = new System.Windows.Forms.Button();
+			this.BtnBaseMvBck = new System.Windows.Forms.Button();
+			this.BtnBaseMvStop = new System.Windows.Forms.Button();
+			this.ImgBaseStop = new System.Windows.Forms.PictureBox();
+			this.ImgBaseRotateL = new System.Windows.Forms.PictureBox();
+			this.ImgBaseRorateR = new System.Windows.Forms.PictureBox();
+			this.ImgBaseTurnL = new System.Windows.Forms.PictureBox();
+			this.ImgBaseTurnR = new System.Windows.Forms.PictureBox();
+			this.ImgBaseMvBck = new System.Windows.Forms.PictureBox();
+			this.BtnInteractive = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.ImgLightFrontLeftOff)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.ImgLightFrontLeftOn)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.ImgBody)).BeginInit();
@@ -46,6 +61,13 @@
 			((System.ComponentModel.ISupportInitialize)(this.ImgBumperFrontRight)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.ImgLightFrontRightOff)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.ImgLightFrontRightOn)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.ImgBaseMvFwd)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.ImgBaseStop)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.ImgBaseRotateL)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.ImgBaseRorateR)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.ImgBaseTurnL)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.ImgBaseTurnR)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.ImgBaseMvBck)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// LblConnStatus
@@ -110,7 +132,7 @@
 			// ImgBumperFrontLeft
 			// 
 			this.ImgBumperFrontLeft.Image = ((System.Drawing.Image)(resources.GetObject("ImgBumperFrontLeft.Image")));
-			this.ImgBumperFrontLeft.Location = new System.Drawing.Point(15, 66);
+			this.ImgBumperFrontLeft.Location = new System.Drawing.Point(15, 65);
 			this.ImgBumperFrontLeft.Name = "ImgBumperFrontLeft";
 			this.ImgBumperFrontLeft.Size = new System.Drawing.Size(64, 30);
 			this.ImgBumperFrontLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -121,7 +143,7 @@
 			// 
 			this.ImgBumperFrontRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.ImgBumperFrontRight.Image = ((System.Drawing.Image)(resources.GetObject("ImgBumperFrontRight.Image")));
-			this.ImgBumperFrontRight.Location = new System.Drawing.Point(323, 66);
+			this.ImgBumperFrontRight.Location = new System.Drawing.Point(323, 65);
 			this.ImgBumperFrontRight.Name = "ImgBumperFrontRight";
 			this.ImgBumperFrontRight.Size = new System.Drawing.Size(64, 30);
 			this.ImgBumperFrontRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -203,11 +225,186 @@
 			this.BtnLightFrontOff.UseVisualStyleBackColor = true;
 			this.BtnLightFrontOff.Click += new System.EventHandler(this.BtnLightFrontOff_Click);
 			// 
+			// BtnBaseMvFwd
+			// 
+			this.BtnBaseMvFwd.Image = ((System.Drawing.Image)(resources.GetObject("BtnBaseMvFwd.Image")));
+			this.BtnBaseMvFwd.Location = new System.Drawing.Point(181, 193);
+			this.BtnBaseMvFwd.Name = "BtnBaseMvFwd";
+			this.BtnBaseMvFwd.Size = new System.Drawing.Size(38, 38);
+			this.BtnBaseMvFwd.TabIndex = 16;
+			this.BtnBaseMvFwd.UseVisualStyleBackColor = true;
+			this.BtnBaseMvFwd.Click += new System.EventHandler(this.BtnBaseMvFwd_Click);
+			// 
+			// ImgBaseMvFwd
+			// 
+			this.ImgBaseMvFwd.BackColor = System.Drawing.Color.White;
+			this.ImgBaseMvFwd.Image = ((System.Drawing.Image)(resources.GetObject("ImgBaseMvFwd.Image")));
+			this.ImgBaseMvFwd.Location = new System.Drawing.Point(166, 237);
+			this.ImgBaseMvFwd.Name = "ImgBaseMvFwd";
+			this.ImgBaseMvFwd.Size = new System.Drawing.Size(68, 120);
+			this.ImgBaseMvFwd.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.ImgBaseMvFwd.TabIndex = 17;
+			this.ImgBaseMvFwd.TabStop = false;
+			// 
+			// BtnBaseMvTurnL
+			// 
+			this.BtnBaseMvTurnL.Image = ((System.Drawing.Image)(resources.GetObject("BtnBaseMvTurnL.Image")));
+			this.BtnBaseMvTurnL.Location = new System.Drawing.Point(96, 193);
+			this.BtnBaseMvTurnL.Name = "BtnBaseMvTurnL";
+			this.BtnBaseMvTurnL.Size = new System.Drawing.Size(38, 38);
+			this.BtnBaseMvTurnL.TabIndex = 18;
+			this.BtnBaseMvTurnL.UseVisualStyleBackColor = true;
+			this.BtnBaseMvTurnL.Click += new System.EventHandler(this.BtnBaseMvTurnL_Click);
+			// 
+			// BtnBaseMvTurnR
+			// 
+			this.BtnBaseMvTurnR.Image = ((System.Drawing.Image)(resources.GetObject("BtnBaseMvTurnR.Image")));
+			this.BtnBaseMvTurnR.Location = new System.Drawing.Point(266, 193);
+			this.BtnBaseMvTurnR.Name = "BtnBaseMvTurnR";
+			this.BtnBaseMvTurnR.Size = new System.Drawing.Size(38, 38);
+			this.BtnBaseMvTurnR.TabIndex = 19;
+			this.BtnBaseMvTurnR.UseVisualStyleBackColor = true;
+			this.BtnBaseMvTurnR.Click += new System.EventHandler(this.BtnBaseMvTurnR_Click);
+			// 
+			// BtnBaseMvRotateR
+			// 
+			this.BtnBaseMvRotateR.Image = ((System.Drawing.Image)(resources.GetObject("BtnBaseMvRotateR.Image")));
+			this.BtnBaseMvRotateR.Location = new System.Drawing.Point(266, 276);
+			this.BtnBaseMvRotateR.Name = "BtnBaseMvRotateR";
+			this.BtnBaseMvRotateR.Size = new System.Drawing.Size(38, 38);
+			this.BtnBaseMvRotateR.TabIndex = 20;
+			this.BtnBaseMvRotateR.UseVisualStyleBackColor = true;
+			this.BtnBaseMvRotateR.Click += new System.EventHandler(this.BtnBaseMvRotateR_Click);
+			// 
+			// BtnBaseMvRotateL
+			// 
+			this.BtnBaseMvRotateL.Image = ((System.Drawing.Image)(resources.GetObject("BtnBaseMvRotateL.Image")));
+			this.BtnBaseMvRotateL.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.BtnBaseMvRotateL.Location = new System.Drawing.Point(96, 276);
+			this.BtnBaseMvRotateL.Name = "BtnBaseMvRotateL";
+			this.BtnBaseMvRotateL.Size = new System.Drawing.Size(38, 38);
+			this.BtnBaseMvRotateL.TabIndex = 21;
+			this.BtnBaseMvRotateL.UseVisualStyleBackColor = true;
+			this.BtnBaseMvRotateL.Click += new System.EventHandler(this.BtnBaseMvRotateL_Click);
+			// 
+			// BtnBaseMvBck
+			// 
+			this.BtnBaseMvBck.Image = ((System.Drawing.Image)(resources.GetObject("BtnBaseMvBck.Image")));
+			this.BtnBaseMvBck.Location = new System.Drawing.Point(181, 363);
+			this.BtnBaseMvBck.Name = "BtnBaseMvBck";
+			this.BtnBaseMvBck.Size = new System.Drawing.Size(38, 38);
+			this.BtnBaseMvBck.TabIndex = 22;
+			this.BtnBaseMvBck.UseVisualStyleBackColor = true;
+			this.BtnBaseMvBck.Click += new System.EventHandler(this.BtnBaseMvBck_Click);
+			// 
+			// BtnBaseMvStop
+			// 
+			this.BtnBaseMvStop.Image = ((System.Drawing.Image)(resources.GetObject("BtnBaseMvStop.Image")));
+			this.BtnBaseMvStop.Location = new System.Drawing.Point(181, 407);
+			this.BtnBaseMvStop.Name = "BtnBaseMvStop";
+			this.BtnBaseMvStop.Size = new System.Drawing.Size(38, 38);
+			this.BtnBaseMvStop.TabIndex = 23;
+			this.BtnBaseMvStop.UseVisualStyleBackColor = true;
+			this.BtnBaseMvStop.Click += new System.EventHandler(this.BtnBaseMvStop_Click);
+			// 
+			// ImgBaseStop
+			// 
+			this.ImgBaseStop.BackColor = System.Drawing.Color.White;
+			this.ImgBaseStop.Image = ((System.Drawing.Image)(resources.GetObject("ImgBaseStop.Image")));
+			this.ImgBaseStop.Location = new System.Drawing.Point(140, 237);
+			this.ImgBaseStop.Name = "ImgBaseStop";
+			this.ImgBaseStop.Size = new System.Drawing.Size(120, 120);
+			this.ImgBaseStop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.ImgBaseStop.TabIndex = 24;
+			this.ImgBaseStop.TabStop = false;
+			// 
+			// ImgBaseRotateL
+			// 
+			this.ImgBaseRotateL.BackColor = System.Drawing.Color.White;
+			this.ImgBaseRotateL.Image = ((System.Drawing.Image)(resources.GetObject("ImgBaseRotateL.Image")));
+			this.ImgBaseRotateL.Location = new System.Drawing.Point(140, 237);
+			this.ImgBaseRotateL.Name = "ImgBaseRotateL";
+			this.ImgBaseRotateL.Size = new System.Drawing.Size(120, 120);
+			this.ImgBaseRotateL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.ImgBaseRotateL.TabIndex = 25;
+			this.ImgBaseRotateL.TabStop = false;
+			// 
+			// ImgBaseRorateR
+			// 
+			this.ImgBaseRorateR.BackColor = System.Drawing.Color.White;
+			this.ImgBaseRorateR.Image = ((System.Drawing.Image)(resources.GetObject("ImgBaseRorateR.Image")));
+			this.ImgBaseRorateR.Location = new System.Drawing.Point(140, 237);
+			this.ImgBaseRorateR.Name = "ImgBaseRorateR";
+			this.ImgBaseRorateR.Size = new System.Drawing.Size(120, 120);
+			this.ImgBaseRorateR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.ImgBaseRorateR.TabIndex = 26;
+			this.ImgBaseRorateR.TabStop = false;
+			// 
+			// ImgBaseTurnL
+			// 
+			this.ImgBaseTurnL.BackColor = System.Drawing.Color.White;
+			this.ImgBaseTurnL.Image = ((System.Drawing.Image)(resources.GetObject("ImgBaseTurnL.Image")));
+			this.ImgBaseTurnL.Location = new System.Drawing.Point(166, 237);
+			this.ImgBaseTurnL.Name = "ImgBaseTurnL";
+			this.ImgBaseTurnL.Size = new System.Drawing.Size(68, 120);
+			this.ImgBaseTurnL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.ImgBaseTurnL.TabIndex = 27;
+			this.ImgBaseTurnL.TabStop = false;
+			// 
+			// ImgBaseTurnR
+			// 
+			this.ImgBaseTurnR.BackColor = System.Drawing.Color.White;
+			this.ImgBaseTurnR.Image = ((System.Drawing.Image)(resources.GetObject("ImgBaseTurnR.Image")));
+			this.ImgBaseTurnR.Location = new System.Drawing.Point(166, 237);
+			this.ImgBaseTurnR.Name = "ImgBaseTurnR";
+			this.ImgBaseTurnR.Size = new System.Drawing.Size(68, 120);
+			this.ImgBaseTurnR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.ImgBaseTurnR.TabIndex = 28;
+			this.ImgBaseTurnR.TabStop = false;
+			// 
+			// ImgBaseMvBck
+			// 
+			this.ImgBaseMvBck.BackColor = System.Drawing.Color.White;
+			this.ImgBaseMvBck.Image = ((System.Drawing.Image)(resources.GetObject("ImgBaseMvBck.Image")));
+			this.ImgBaseMvBck.Location = new System.Drawing.Point(166, 237);
+			this.ImgBaseMvBck.Name = "ImgBaseMvBck";
+			this.ImgBaseMvBck.Size = new System.Drawing.Size(68, 120);
+			this.ImgBaseMvBck.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.ImgBaseMvBck.TabIndex = 29;
+			this.ImgBaseMvBck.TabStop = false;
+			// 
+			// BtnInteractive
+			// 
+			this.BtnInteractive.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.BtnInteractive.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.BtnInteractive.ForeColor = System.Drawing.Color.Red;
+			this.BtnInteractive.Location = new System.Drawing.Point(337, 4);
+			this.BtnInteractive.Name = "BtnInteractive";
+			this.BtnInteractive.Size = new System.Drawing.Size(22, 23);
+			this.BtnInteractive.TabIndex = 30;
+			this.BtnInteractive.Text = "i";
+			this.BtnInteractive.UseVisualStyleBackColor = true;
+			this.BtnInteractive.Click += new System.EventHandler(this.BtnInteractive_Click);
+			// 
 			// FrmMainWindow
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(399, 550);
+			this.Controls.Add(this.BtnInteractive);
+			this.Controls.Add(this.ImgBaseMvBck);
+			this.Controls.Add(this.ImgBaseTurnR);
+			this.Controls.Add(this.ImgBaseTurnL);
+			this.Controls.Add(this.ImgBaseMvFwd);
+			this.Controls.Add(this.ImgBaseRorateR);
+			this.Controls.Add(this.ImgBaseRotateL);
+			this.Controls.Add(this.BtnBaseMvStop);
+			this.Controls.Add(this.BtnBaseMvBck);
+			this.Controls.Add(this.BtnBaseMvRotateL);
+			this.Controls.Add(this.BtnBaseMvRotateR);
+			this.Controls.Add(this.BtnBaseMvTurnR);
+			this.Controls.Add(this.BtnBaseMvTurnL);
+			this.Controls.Add(this.BtnBaseMvFwd);
 			this.Controls.Add(this.BtnLightFrontOff);
 			this.Controls.Add(this.BtnLightFrontOn);
 			this.Controls.Add(this.BtnAbout);
@@ -220,15 +417,19 @@
 			this.Controls.Add(this.BtnLightFrontLeftOn);
 			this.Controls.Add(this.LblConnStatus);
 			this.Controls.Add(this.ImgLightFrontLeftOn);
-			this.Controls.Add(this.ImgBody);
 			this.Controls.Add(this.ImgLightFrontRightOff);
 			this.Controls.Add(this.ImgLightFrontRightOn);
+			this.Controls.Add(this.ImgBaseStop);
+			this.Controls.Add(this.ImgBody);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.KeyPreview = true;
 			this.MaximizeBox = false;
 			this.Name = "FrmMainWindow";
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
 			this.Text = "PCBot";
 			this.Load += new System.EventHandler(this.MainWindow_Load);
 			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmMainWindow_FormClosed);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmMainWindow_KeyDown);
 			((System.ComponentModel.ISupportInitialize)(this.ImgLightFrontLeftOff)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.ImgLightFrontLeftOn)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.ImgBody)).EndInit();
@@ -236,6 +437,13 @@
 			((System.ComponentModel.ISupportInitialize)(this.ImgBumperFrontRight)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.ImgLightFrontRightOff)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.ImgLightFrontRightOn)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.ImgBaseMvFwd)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.ImgBaseStop)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.ImgBaseRotateL)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.ImgBaseRorateR)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.ImgBaseTurnL)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.ImgBaseTurnR)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.ImgBaseMvBck)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -258,6 +466,21 @@
 		private System.Windows.Forms.Button BtnAbout;
 		private System.Windows.Forms.Button BtnLightFrontOn;
 		private System.Windows.Forms.Button BtnLightFrontOff;
+		private System.Windows.Forms.Button BtnBaseMvFwd;
+		private System.Windows.Forms.PictureBox ImgBaseMvFwd;
+		private System.Windows.Forms.Button BtnBaseMvTurnL;
+		private System.Windows.Forms.Button BtnBaseMvTurnR;
+		private System.Windows.Forms.Button BtnBaseMvRotateR;
+		private System.Windows.Forms.Button BtnBaseMvRotateL;
+		private System.Windows.Forms.Button BtnBaseMvBck;
+		private System.Windows.Forms.Button BtnBaseMvStop;
+		private System.Windows.Forms.PictureBox ImgBaseStop;
+		private System.Windows.Forms.PictureBox ImgBaseRotateL;
+		private System.Windows.Forms.PictureBox ImgBaseRorateR;
+		private System.Windows.Forms.PictureBox ImgBaseTurnL;
+		private System.Windows.Forms.PictureBox ImgBaseTurnR;
+		private System.Windows.Forms.PictureBox ImgBaseMvBck;
+		private System.Windows.Forms.Button BtnInteractive;
 	}
 }
 
--- Examples/PCBot/PCBot_PCApp/PCBot_PCApp/FrmMainWindow.cs
+++ Examples/PCBot/PCBot_PCApp/PCBot_PCApp/FrmMainWindow.cs
@@ -2,35 +2,28 @@
 using System.Windows.Forms;
 using SWF = System.Windows.Forms;
 using System.Drawing;
+using System.Threading;
 
 namespace PCBot_PCApp {
 	public partial class FrmMainWindow:Form {
-		/// <summary>
-		/// Timer which tries to reconnect after a connection problem
-		/// </summary>
-		SWF.Timer ConnectionTimer = new SWF.Timer();
+		SWF.Timer EventsTimer = new SWF.Timer();
+		ControlByteBaseMv BaseMvCurrentStatus = ControlByteBaseMv.Stop;
+		bool LightFrontLeftON = false;
+		bool LightFrontRightON = false;
 
 		public FrmMainWindow() {
 			InitializeComponent();
 			Global.MainWindow = this;
 		}
 
-		void ConnectionTimer_Tick(object sender, EventArgs e) {
-			if(Communication.Status == Status.Disconnected) Communication.Connect();
-		}
-
 		public void SetConnectionStatus(Status st) {
-			switch(st) {
-				case Status.Connected:
-					LblConnStatus.Text = "Connected";
-					LblConnStatus.ForeColor = Color.Green;
-					break;
-				case Status.Disconnected:
-					LblConnStatus.Text = "Disconnected";
-					LblConnStatus.ForeColor = Color.Red;
-					break;
-				default:
-					throw new NotImplementedException();
+			if(st == Status.Connected) {
+				LblConnStatus.Text = "Connected";
+				LblConnStatus.ForeColor = Color.Green;
+			} else {
+				LblConnStatus.Text = "Disconnected";
+				LblConnStatus.ForeColor = Color.Red;
+				SetDefaults();
 			}
 		}
 
@@ -54,35 +47,75 @@ namespace PCBot_PCApp {
 			}
 		}
 
+		public void SetBaseMvStatus(ControlByteBaseMv BaseMvStatus) {
+			BaseMvCurrentStatus = BaseMvStatus;
+			ImgBaseMvFwd.Hide();
+			ImgBaseStop.Hide();
+			ImgBaseMvBck.Hide();
+			ImgBaseTurnL.Hide();
+			ImgBaseTurnR.Hide();
+			ImgBaseRotateL.Hide();
+			ImgBaseRorateR.Hide();
+			switch(BaseMvStatus) {
+				case ControlByteBaseMv.Forward:
+					ImgBaseMvFwd.Show();
+					break;
+				case ControlByteBaseMv.Stop:
+					ImgBaseStop.Show();
+					break;
+				case ControlByteBaseMv.Backwards:
+					ImgBaseMvBck.Show();
+					break;
+				case ControlByteBaseMv.TurnLeft:
+					ImgBaseTurnL.Show();
+					break;
+				case ControlByteBaseMv.TurnRight:
+					ImgBaseTurnR.Show();
+					break;
+				case ControlByteBaseMv.RotateLeft:
+					ImgBaseRotateL.Show();
+					break;
+				case ControlByteBaseMv.RotateRight:
+					ImgBaseRorateR.Show();
+					break;
+				default:
+					Global.Log("Unknown base movement status: " + BaseMvStatus.ToString());
+					break;
+			}
+		}
+
 		private void MainWindow_Load(object sender, EventArgs e) {
-			// set default values and messages
-			SetConnectionStatus(Communication.Status);
+			SetConnectionStatus(Status.Disconnected); //it also sets all default values
+			Communication.Connect();
+			EventsTimer.Tick += new EventHandler(EventsTimer_Tick);
+			EventsTimer.Interval = 100;
+			EventsTimer.Start();
+		}
+
+		private void SetDefaults() {
 			ImgBumperFrontLeft.Hide();
 			ImgBumperFrontRight.Hide();
 			ImgLightFrontLeftOff.BringToFront();
 			ImgLightFrontRightOff.BringToFront();
-
-			ConnectionTimer.Tick += new EventHandler(ConnectionTimer_Tick);
-			ConnectionTimer.Interval = 1000;
-			ConnectionTimer.Start();
+			SetBaseMvStatus(ControlByteBaseMv.Stop);
 		}
 
 		private void BtnLightFrontLeftOn_Click(object sender, EventArgs e) {
 			Global.Log("Turning ON front left light");
 			ImgLightFrontLeftOn.BringToFront();
 			Communication.SetLight(ControlByteLight.FrontLeftOn);
+			LightFrontLeftON = true;
 		}
 
 		private void BtnLightFrontLeftOff_Click(object sender, EventArgs e) {
 			Global.Log("Turning OFF front left light");
 			ImgLightFrontLeftOff.BringToFront();
 			Communication.SetLight(ControlByteLight.FrontLeftOff);
+			LightFrontLeftON = false;
 		}
 
 		private void FrmMainWindow_FormClosed(object sender, FormClosedEventArgs e) {
-			ConnectionTimer.Stop();
-			ConnectionTimer.Dispose();
-			ConnectionTimer = null;
+			Global.Log("Closing main window");
 			Communication.Disconnect();
 		}
 
@@ -95,22 +128,146 @@ namespace PCBot_PCApp {
 			Global.Log("Turning ON front right light");
 			ImgLightFrontRightOn.BringToFront();
 			Communication.SetLight(ControlByteLight.FrontRightOn);
+			LightFrontRightON = true;
 		}
 
 		private void BtnLightFrontRightOff_Click(object sender, EventArgs e) {
 			Global.Log("Turning OFF front right light");
 			ImgLightFrontRightOff.BringToFront();
 			Communication.SetLight(ControlByteLight.FrontRightOff);
+			LightFrontRightON = false;
 		}
 
 		private void BtnLightFrontOn_Click(object sender, EventArgs e) {
 			BtnLightFrontLeftOn.PerformClick();
+			Thread.Sleep(50);
 			BtnLightFrontRightOn.PerformClick();
 		}
 
 		private void BtnLightFrontOff_Click(object sender, EventArgs e) {
 			BtnLightFrontLeftOff.PerformClick();
+			Thread.Sleep(50);
 			BtnLightFrontRightOff.PerformClick();
+		}
+
+		private void BtnBaseMvFwd_Click(object sender, EventArgs e) {
+			Communication.SetBaseMv(ControlByteBaseMv.Forward);
+		}
+
+		private void BtnBaseMvStop_Click(object sender, EventArgs e) {
+			Communication.SetBaseMv(ControlByteBaseMv.Stop);
+		}
+
+		private void BtnBaseMvBck_Click(object sender, EventArgs e) {
+			Communication.SetBaseMv(ControlByteBaseMv.Backwards);
+		}
+
+		private void BtnBaseMvTurnL_Click(object sender, EventArgs e) {
+			Communication.SetBaseMv(ControlByteBaseMv.TurnLeft);
+		}
+
+		private void BtnBaseMvTurnR_Click(object sender, EventArgs e) {
+			Communication.SetBaseMv(ControlByteBaseMv.TurnRight);
+		}
+
+		private void BtnBaseMvRotateL_Click(object sender, EventArgs e) {
+			Communication.SetBaseMv(ControlByteBaseMv.RotateLeft);
+		}
+
+		private void BtnBaseMvRotateR_Click(object sender, EventArgs e) {
+			Communication.SetBaseMv(ControlByteBaseMv.RotateRight);
+		}
+
+		/// <summary>
+		/// Mouse threshold
+		/// </summary>
+		const int thr = 10;
+
+		/// <summary>
+		/// Previous mouse position
+		/// </summary>
+		Point PrevMouse;
+
+		bool InteractiveMode {
+			get {
+				return _InteractiveMode;
+			}
+			set {
+				if(value) BtnInteractive.ForeColor = Color.Green;
+				else BtnInteractive.ForeColor = Color.Red;
+				_InteractiveMode = value;
+			}
+		}
+		bool _InteractiveMode = false;
+
+		void EventsTimer_Tick(object sender, EventArgs e) {
+			if(PrevMouse == null) PrevMouse = MousePosition;
+			if(InteractiveMode) {
+				if(MousePosition.Y < PrevMouse.Y - thr) BaseMvFwd();
+				if(MousePosition.Y > PrevMouse.Y + thr) BaseMvBck();
+				if(MousePosition.X < PrevMouse.X - thr) BaseMvLeft();
+				if(MousePosition.X > PrevMouse.X + thr) BaseMvRight();
+			}
+			PrevMouse = new Point(MousePosition.X, MousePosition.Y);
+		}
+
+		void BaseMvFwd() {
+			if(BaseMvCurrentStatus == ControlByteBaseMv.Backwards) BtnBaseMvStop.PerformClick();
+			else if(BaseMvCurrentStatus == ControlByteBaseMv.RotateLeft) BtnBaseMvTurnL.PerformClick();
+			else if(BaseMvCurrentStatus == ControlByteBaseMv.RotateRight) BtnBaseMvTurnR.PerformClick();
+			else BtnBaseMvFwd.PerformClick();
+		}
+
+		void BaseMvBck() {
+			if(BaseMvCurrentStatus == ControlByteBaseMv.Forward) BtnBaseMvStop.PerformClick();
+			else BtnBaseMvBck.PerformClick();
+		}
+
+		void BaseMvLeft() {
+			if(BaseMvCurrentStatus == ControlByteBaseMv.TurnRight) BtnBaseMvFwd.PerformClick();
+			else if(BaseMvCurrentStatus == ControlByteBaseMv.Forward) BtnBaseMvTurnL.PerformClick();
+			else if(BaseMvCurrentStatus == ControlByteBaseMv.RotateRight) BtnBaseMvStop.PerformClick();
+			else BtnBaseMvRotateL.PerformClick();
+		}
+
+		void BaseMvRight() {
+			if(BaseMvCurrentStatus == ControlByteBaseMv.TurnLeft) BtnBaseMvFwd.PerformClick();
+			else if(BaseMvCurrentStatus == ControlByteBaseMv.Forward) BtnBaseMvTurnR.PerformClick();
+			else if(BaseMvCurrentStatus == ControlByteBaseMv.RotateLeft) BtnBaseMvStop.PerformClick();
+			else BtnBaseMvRotateR.PerformClick();
+		}
+
+		private void BtnInteractive_Click(object sender, EventArgs e) {
+			// switch interactive/normal mode
+			InteractiveMode = !InteractiveMode;
+		}
+
+		private void FrmMainWindow_KeyDown(object sender, KeyEventArgs e) {
+			switch(e.KeyCode) {
+				case Keys.Escape:
+					BtnBaseMvStop.PerformClick();
+					break;
+				case Keys.F5:
+					InteractiveMode = !InteractiveMode;
+					break;
+				case Keys.L:
+				case Keys.Space:
+					if(LightFrontLeftON && LightFrontRightON) BtnLightFrontOff.PerformClick();
+					else BtnLightFrontOn.PerformClick();
+					break;
+				case Keys.W:
+					BaseMvFwd();
+					break;
+				case Keys.S:
+					BaseMvBck();
+					break;
+				case Keys.A:
+					BaseMvLeft();
+					break;
+				case Keys.D:
+					BaseMvRight();
+					break;
+			}
 		}
 	}
 }
