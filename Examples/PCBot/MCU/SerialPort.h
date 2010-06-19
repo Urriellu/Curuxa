302f07a4d4591a7653b630b968d1a2d9d20058d0
urriellu <urriellu@curuxa.org>
2010-06-19 10:27:03 +0000

    Clearing error more often.

--- Examples/PCBot/MCU/SerialPort.h
+++ Examples/PCBot/MCU/SerialPort.h
@@ -1,18 +1,24 @@
-﻿//Reads a byte from the serial port. If there is no data on the receiver buffer, wait indefinitely for the next incoming byte
-unsigned int8 ReadSPWait(){
+﻿// Clear UART errors
+void ClearErrors(){
 	unsigned int8 dummy;
+
+	if (OERR) {
+		TXEN=0;
+		TXEN=1;
+		CREN=0;
+		CREN=1;
+	}
+	if (FERR) {
+		dummy=RCREG;
+		TXEN=0;
+		TXEN=1;
+	}
+}
+
+//Reads a byte from the serial port. If there is no data on the receiver buffer, wait indefinitely for the next incoming byte
+unsigned int8 ReadSPWait(){
 	while(!RCIF) {
-		if (OERR) {
-			TXEN=0;
-			TXEN=1;
-			CREN=0;
-			CREN=1;
-		}
-		if (FERR) {
-			dummy=RCREG;
-			TXEN=0;
-			TXEN=1;
-		}
+		ClearErrors();
 	}
 	return RCREG;
 }
@@ -20,7 +26,10 @@ unsigned int8 ReadSPWait(){
 //Reads a byte from the serial port. If there is no data on the receiver buffer, return 0xFF
 unsigned int8 ReadSP() {
 	if(RCIF) return RCREG;
-	else return 0xFF;
+	else {
+		ClearErrors();
+		return 0xFF;
+	}
 }
 
 //Writes a byte to the serial port
