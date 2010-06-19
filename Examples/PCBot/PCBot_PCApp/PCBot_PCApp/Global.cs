5fa0bc83319ef16a6f9e0c447159a0bb2be1619e
urriellu <urriellu@curuxa.org>
2010-06-19 10:30:16 +0000

    Log() fixed.

--- Examples/PCBot/PCBot_PCApp/PCBot_PCApp/Global.cs
+++ Examples/PCBot/PCBot_PCApp/PCBot_PCApp/Global.cs
@@ -10,7 +10,7 @@ namespace PCBot_PCApp {
 		public static void Log(string p) {
 			string LogText = p;
 			if(p[0] != '[') LogText = string.Format("[{0:00}:{1:00}:{2:00}.{3:000}] {4}", DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second, DateTime.Now.Millisecond, p);
-			Console.WriteLine(p);
+			Console.WriteLine(LogText);
 			//if(MainWindow != null) MainWindow.TxtLog.AppendText(p);
 		}
 	}
