f18a883d62f109875842b009e63a3cec26490658
urriellu <urriellu@curuxa.org>
2011-05-05 15:41:11 +0000

    Some new settings

--- Examples/3DScanner/3DScannerPC/3DScannerPC/Settings.cs
+++ Examples/3DScanner/3DScannerPC/3DScannerPC/Settings.cs
@@ -5,10 +5,17 @@ using System.Text;
 
 namespace _3DScannerPC {
 	public static class Settings {
-		//public static string DefaultSerialPortName = "COM0";
-		//public static int BaudRate = 9600;
 		public static byte ScannerID = 97;
 
 		public static int AdcMax = 1024;
+
+		/// <summary>
+		/// Minimum amount of milliseconds between data sent
+		/// </summary>
+		public static int SendDelayThreshold = 10;
+
+		public static bool RequireAuthentication = false;
+
+		public static UInt16 T1preload = 0xA23F;
 	}
 }
